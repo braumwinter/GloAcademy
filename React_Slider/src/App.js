c9c068553c3bc95e0fa40669b47f9efa5cce29bb
Kate <braumwinter@gmail.com>
2019-01-12 22:21:32 +0200

    Update App.js

--- React_Slider/src/App.js
+++ React_Slider/src/App.js
@@ -3,19 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWraper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
-    return (
-      <div className="wrapper">
-      <SayFullName name = "Ivan" surname = "Petrov" link = "vk.com" />
-      <SayFullName name = "Anna" surname = "Karerina" link = "facebook.com" />
-      <SayFullName name = "Pit" surname = "Mike" link = "#" />
+    return(
+      <div className = "App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWraper>
+          <Grid>
+            <Main /> 
+          </Grid>
+        </MainWraper>               
       </div>
-    );
+      );
   }
 }
 
